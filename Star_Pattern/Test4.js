96f3e919c221bd3ae54cb208535e4cea23071fd6
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-01-26 00:27:04 +0530

    26-1-2026

--- Star_Pattern/Test4.js
+++ Star_Pattern/Test4.js
@@ -6,12 +6,25 @@
 // 5 5 5 5 5
 
 let n = 5;
+//================================================printRightTriangleWithNumber(rows) Approach 1=================================================
+// function printRightTriangle(n) {
+//   let str = "";
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j <= i; j++) {
+//       str += i + 1;
+//     }
+//     str += "\n";
+//   }
+//   return str;
+// }
+// console.log(printRightTriangle(n));
 
+//================================================printRightTriangleWithNumber(rows) Approach 2=================================================
 function printRightTriangle(n) {
   let str = "";
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j <= i; j++) {
-      str += i + 1;
+  for (let i = 1; i <= n; i++) {
+    for (let j = 1; j <= i; j++) {
+      str += i;
     }
     str += "\n";
   }
