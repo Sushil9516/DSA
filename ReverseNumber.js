401a8fb2d01e172a3991c958d0594e639f68f2cf
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-01-29 01:45:48 +0530

    29-1-2026

---
+++ ReverseNumber.js
@@ -0,0 +1,10 @@
+function reverseNumber(num){
+    let temp = num;
+    if(temp<0) temp=-temp;
+    if(temp >=0 && temp<10) return num;
+
+    for(let i=temp; i>0; i=Math.trunc(i/10))
+    {
+        
+    }
+}
