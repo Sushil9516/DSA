bb27807b1708d08703f5baffd84a98ef570af4df
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-01-31 01:39:45 +0530

    isPalindrome is solved

---
+++ isPalindrome.js
@@ -0,0 +1,11 @@
+function isPalindrome(x) {
+  let temp = x;
+  let rev = 0;
+  if (temp < 0) temp = -temp;
+  if (temp >= 0 && temp < 10) return x;
+  if (!x) return "Please give me any number";
+  for (let i = temp; i > 0; i = Math.trunc(i / 10)) {
+    rev = rev * 10 + (i % 10);
+  }
+  return x == rev;
+}
