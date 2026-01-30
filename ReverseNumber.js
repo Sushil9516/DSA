110a7fc97d02547d687b811837031bd5de4cbb15
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-01-31 00:28:57 +0530

    31-1-2026

--- ReverseNumber.js
+++ ReverseNumber.js
@@ -1,10 +1,15 @@
-function reverseNumber(num){
-    let temp = num;
-    if(temp<0) temp=-temp;
-    if(temp >=0 && temp<10) return num;
-
-    for(let i=temp; i>0; i=Math.trunc(i/10))
-    {
-        
-    }
+function reverseNumber(num) {
+  let temp = num;
+  let rev = 0;
+  if (temp < 0) temp = -temp;
+  if (temp >= 0 && temp < 10) return num;
+  if (!num) return "Please give me any number";
+  for (let i = temp; i > 0; i = Math.trunc(i / 10)) {
+    rev = rev * 10 + (i % 10);
+  }
+  if (num < 0) return -rev;
+  else return rev; 
 }
+let num = -289;
+console.log(reverseNumber(num));
+
