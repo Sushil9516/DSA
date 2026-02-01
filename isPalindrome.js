f732bf807498da3a5ceae81c3ad13a3c068c1d4a
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-02-01 22:07:21 +0530

    2-1-2026

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,6 +1,8 @@
 function isPalindrome(x) {
+
   let temp = x;
   let rev = 0;
+  
   if (temp < 0) temp = -temp;
   if (temp >= 0 && temp < 10) return x;
   if (!x) return "Please give me any number";
