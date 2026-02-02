08fca91272350de681af56ffeebcbfa0d6dbdd05
Sushil Kumar Soni <sonisushil9516@gmail.com>
2026-02-02 23:25:01 +0530

    2-2-2026

---
+++ test.js
@@ -0,0 +1,12 @@
+// let arr=[1,2,34,5]
+// arr.length=1
+// console.log(arr)
+
+// let arr = [1, 2, 3, 4, 5];
+// delete arr[2];
+// console.log(arr.length);
+// console.log(arr);
+
+let arr = [1, 2, 3, 45, 6];
+arr.splice(1, 2, 90, 80);
+console.log(arr);
